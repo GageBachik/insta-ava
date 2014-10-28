cadcf98085286d0c830d672f33806f3574dbc70a
gbachik <gbachik@gmail.com>
2014-10-27 22:31:56 -0600

    added creds

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://insta-ava:freedom347@ds049150.mongolab.com:49150/insta-ava');
 Instagram = require('instagram-node-lib');
 
 var app = express();
@@ -8,35 +10,43 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-
+var IMAGE_TAG = 'blue'
 Instagram.set('client_id', '3c91d050cfd749178805d7660b1499ca');
 Instagram.set('client_secret', 'adaa5118a8df4e169c37734156302799');
 
-app.get('/', function(req, res) {
-	res.render('index');
-});
-
-app.get('/admin', function(req, res) {
-	var pass = req.query;
-	if (pass === 'password') {
-		res.render('admin');
-	}else{
-		res.send('INCORRECT PASSWORD!')
-	}
-});
-
-app.get('/feed', function(req, res) {
+var getAllowedImages = function(tag, view){
 	Instagram.tags.recent({
-		name: 'blue',
+		name: tag,
 		complete: function(data){
 			var json = [];
 			data.map(function(imagObj, index){
 				var jsonObj = {id: imagObj.id, image: imagObj.images.low_resolution.url};
 				json.push(jsonObj);
 			});
-			res.send(json);
+			if (view) {
+				res.render(view, json);
+			}else{
+				res.send(json);
+			}
 		}
 	});
+}
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/admin', function(req, res) {
+	var pass = req.query.password;
+	if (pass === 'password') {
+		getAllowedImages(IMAGE_TAG, 'admin');
+	}else{
+		res.send('INCORRECT PASSWORD!')
+	}
+});
+
+app.get('/feed', function(req, res) {
+	getAllowedImages(IMAGE_TAG)
 });
 
 var server = app.listen(3000, function() {
